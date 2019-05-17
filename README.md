934ff5143e5e51dbe717ece1b8296d77f788aa98
Ikenna-hmcts <ikenna.ibeanu@hmcts.net>
2019-05-17 13:07:31 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # convert-numbers-to-english
 Convert given number to English
+
+App has been setup as Spring boot webapp with a UI to enter number to convert.
+
+## Prerequisites
+Run with Gradle
+
--- src/main/java/com/ibctec/numberconverter/NumberConverterApplication.java
+++ src/main/java/com/ibctec/numberconverter/NumberConverterApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NumberConverterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NumberConverterApplication.class, args);
 	}
-
 }
